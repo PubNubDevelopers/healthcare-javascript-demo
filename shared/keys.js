5f44ece6ec69ffd12dfaf2f27f4775e20946da98
darryn.campbell <darryn.campbell@pubnub.com>
2024-06-14 11:18:02 +0100

    Update keys (prod keys are protected by AM)

--- shared/keys.js
+++ shared/keys.js
@@ -3,16 +3,7 @@
  * Please see the ReadMe file for details of where to obtain keys
  */
 
-//const publish_key = '' //  ENTER YOUR PUBLISH KEY HERE
-//const subscribe_key = '' //  ENTER YOUR SUBSCRIBE KEY HERE
-//const geo_key = ''  //  ENTER YOUR GOOGLE API KEY HERE WITH THE FOLLOWING ENABLED: Maps JavaScript API, Geocoding API
+const publish_key = '' //  ENTER YOUR PUBLISH KEY HERE
+const subscribe_key = '' //  ENTER YOUR SUBSCRIBE KEY HERE
+const geo_key = ''  //  ENTER YOUR GOOGLE API KEY HERE WITH THE FOLLOWING ENABLED: Maps JavaScript API, Geocoding API
 
-//  TEST KEYS
-const publish_key = 'pub-c-e94d77e4-f4e1-4c38-bed5-38f59f2442fb' //  ENTER YOUR PUBLISH KEY HERE
-const subscribe_key = 'sub-c-b913edc7-5b9b-49e6-9196-4d6ffe648600' //  ENTER YOUR SUBSCRIBE KEY HERE
-const geo_key = 'AIzaSyC_RWL4-SwR7LyDpIAtKngxiG6WZ844uxQ';
-
-//  ACCESS MANAGER KEYS
-//const publish_key = 'pub-c-021aeb94-fbac-4027-8b68-3111ad09cb83' //  ENTER YOUR PUBLISH KEY HERE
-//const subscribe_key = 'sub-c-80cb998d-5102-4e72-89bf-cd278212bd55' //  ENTER YOUR SUBSCRIBE KEY HERE
-//const geo_key = 'AIzaSyC_RWL4-SwR7LyDpIAtKngxiG6WZ844uxQ';
